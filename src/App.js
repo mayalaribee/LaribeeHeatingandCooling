349d07921eb628a3193165d54015d7a039925ac4
mayalaribee <mayalaribee@gmail.com>
2026-06-05 18:29:54 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 import mayaPhoto from "./assets/maya.jpeg";
-import merricPhoto from "./assests/merric.jpeg";
+import merricPhoto from "./assets/merric.jpeg";
 export default function App() {
   return (
     <div className="site">
